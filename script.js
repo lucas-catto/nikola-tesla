97d0ebae02eb26302f36e4cd6c453edb4301ae89
lucas-catto <lucascattosilva2018@gmail.com>
2023-10-05 20:32:43 -0300

    New version.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
-    fetch('Nikola-Tesla.json')
+    fetch('./JSON/1.json')
 
         .then(response => response.json())
         .then(data => {
@@ -9,11 +9,27 @@ document.addEventListener("DOMContentLoaded", function(){
             let tesla = '';
 
             data.NikolaTesla.forEach(nikola => {
-
                 tesla += ` <img src="${nikola.Image}"> `;
-
             });
-            document.getElementById('main').innerHTML = tesla;
+            document.getElementById('1').innerHTML = tesla;
+        })
+        .catch(error = console.error('Error to load the images: ', error));
+
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    fetch('./JSON/2.json')
+
+        .then(response => response.json())
+        .then(data => {
+        
+            let tesla = '';
+    
+            data.NikolaTesla.forEach(nikola => {
+                tesla += ` <img src="${nikola.Image}"> `;
+            });
+            document.getElementById('2').innerHTML = tesla;
         })
         .catch(error = console.error('Error to load the images: ', error));
 });
